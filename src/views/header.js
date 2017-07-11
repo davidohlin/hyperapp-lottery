ba2416080d0736e0225da18706b06f6711faba97
David Öhlin <hello@davidohlin.se>
2017-07-11 15:07:06 +0200

    updated link in header

--- src/views/header.js
+++ src/views/header.js
@@ -3,5 +3,5 @@ import { h } from 'hyperapp'
 export default () =>
 	<header class="header px2 pt2">
 		<h1 class="small left">Lottery</h1>
-		<small class="right">Created with hyperapp. <a class="btn-alt" href="#">Source</a></small>
+		<small class="right">Created with hyperapp. <a class="btn-alt" href="https://github.com/davidohlin/hyperapp-lottery">Source</a></small>
 	</header>
